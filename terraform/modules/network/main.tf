0976ed6de2e47a4f170d00f994f55b93c518f93d
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-17 21:40:56 -0300

    teste3

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -77,16 +77,3 @@ resource "aws_security_group" "vpc_sg_pub" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
-# --- OUTPUTS ---
-output "vpc_id" {
-  value = aws_vpc.vpc.id
-}
-
-output "subnets" {
-  value = [aws_subnet.sn_pub_az1a.id, aws_subnet.sn_pub_az1b.id]
-}
-
-output "security_group_id" {
-  value = aws_security_group.vpc_sg_pub.id
-}
