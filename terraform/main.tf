6081a75f9648e1977589d5a23d26ea2c4638aecd
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-17 21:49:01 -0300

    teste5

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,6 @@
-provider "aws" { 
-  region                   = "us-east-1"
-  shared_config_files      = [".aws/config"]
-  shared_credentials_files = [".aws/credentials"]
-  profile                  = "iac"
+# --- PROVIDER AWS ---
+provider "aws" {
+  region = "us-east-1"
 }
 
 # --- MÓDULO NETWORK ---
@@ -12,9 +10,9 @@ module "network" {
 
 # --- MÓDULO LOAD BALANCER ---
 module "loadbalancer" {
-  source            = "./modules/loadbalancer"
-  vpc_id            = module.network.vpc_id
-  subnets           = module.network.subnets
-  security_group_id = module.network.security_group_id
-  user_data         = file("./scripts/user_data.sh")
+  source             = "./modules/loadbalancer"
+  vpc_id             = module.network.vpc_id
+  subnets            = module.network.subnets
+  security_group_id  = module.network.security_group_id
+  user_data          = file("./scripts/user_data.sh")
 }
